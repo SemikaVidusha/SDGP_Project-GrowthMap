51e1fbae07ee5f74554f32218de223304dafebdb
kethmi916 <kethmi.20232355@iit.ac.lk>
2026-02-17 21:18:18 +0530

    updated main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Added this import
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+import { AuthProvider } from "./lib/AuthContext";
+
+// We only need ONE createRoot call
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
