c9dc93c8557e6fdee9a4a84dca11b67532397aa9
Sanuji Weerakkody <sanujiweerakkody8@gmail.com>
2026-03-19 15:40:41 +0530

    job.js

---
+++ SDGP/backend/models/Job.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    company: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    requirements: [String],
+    salary: { type: String },
+    postedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Job', jobSchema);
